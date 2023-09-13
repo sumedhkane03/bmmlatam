9ab5ce1e25bd1eac0d43f9b087c0d6859a939276
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-13 14:14:59 -0400

    made profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import {  GoogleAuthProvider, ProviderId, reload, signInWithEmailAndPassword, signInWithEmailLink   } from 'firebase/auth';
-import { auth, signInWithGoogle } from '../Firebase';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { auth } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Navbar from '../components/Navbar';
 
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
 
@@ -14,16 +14,46 @@ import { getAuth } from "firebase/auth";
 
 const Profile = () => {
     const navigate = useNavigate();
+
+    const [displayName, setDisplayName] = useState();
+    const [userEmail,setUserEmail] = useState();
+    const [uid,setUid] = useState();
+
+
     
-       
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/firebase.User
+
+              // ...
+              
+              setDisplayName(user.displayName);
+              setUserEmail(user.email);
+              setUid(user.uid);
+              console.log(user.toJSON());
+            } else {
+              // User is signed out
+              // ...
+              console.log("user is logged out")
+            }
+          });
+         
+    }, []);   
     
  
     return(
         <>
-        {/* <div className='bg-2' id = 'background-3'/> */}
         <div className='bg-2 fade-in' id = 'background-2'/>
                 <Navbar/>
-                <div className='loading-bar load-fast'/> 
+                <div className='loading-bar load-fast'/>
+            
+            <div className='events-page-container'>
+                <p className='events-page-title'>{displayName}</p>
+                <p className='events-page-title'>{userEmail}</p>
+                <p className='events-page-title'>{uid}</p>
+            </div>
 
         </>
     )
