c60b60b62a5dbca85aa8a2cca9130cdfced52c87
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-10-20 17:03:27 -0400

    profile basics,

---
+++ src/pages/OLD - Profile.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import { auth } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+
+
+
+
+const Profile = () => {
+    const navigate = useNavigate();
+
+    const [displayName, setDisplayName] = useState();
+    const [userEmail,setUserEmail] = useState();
+    const [uid,setUid] = useState();
+
+
+    
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/firebase.User
+
+              // ...
+
+              setDisplayName(user.displayName);
+              setUserEmail(user.email);
+              setUid(user.uid);
+            //   console.log(user.toJSON());
+            } else {
+              // User is signed out
+              // ...
+              console.log("user is logged out")
+            }
+          });
+         
+    }, []);   
+    
+ 
+    return(
+        <>
+        <div className='bg-2 fade-in' id = 'background-2'/>
+                <Navbar/>
+                <div className='loading-bar load-fast'/>
+            
+            <div className='events-page-container'>
+                <p className='events-page-title'>{displayName}</p>
+                
+                <p className='events-page-title'>{userEmail}</p>
+                
+                <p className='events-page-title'>{uid}</p>
+            </div>
+            <Footer/>
+        </>
+    )
+}
+ 
+export default Profile
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,65 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from '../Firebase';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-
+import { onAuthStateChanged } from "firebase/auth";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-
-
-
-
+import '../css/Profile.css'; // Import your profile page styles
 
 const Profile = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [displayName, setDisplayName] = useState();
-    const [userEmail,setUserEmail] = useState();
-    const [uid,setUid] = useState();
+  const [displayName, setDisplayName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [uid, setUid] = useState('');
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setDisplayName(user.displayName);
+        setUserEmail(user.email);
+        setUid(user.uid);
+      } else {
+        console.log("user is logged out");
+        // Redirect to the login page or handle the signed-out state as needed
+      }
+    });
+  }, []);
 
-    
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/firebase.User
+  return (
+    <div className="profile-container">
+      <Navbar />
+      <div className="profile-content">
+        <div className="profile-header">
+          <h1 className="profile-title">{displayName}</h1>
+          <p className="profile-email">{userEmail}</p>
+        </div>
+        <div className="profile-details">
+          <p className="profile-uid">User ID: {uid}</p>
+          {/* You can add more user information here, such as a profile picture or bio */}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-              // ...
-
-              setDisplayName(user.displayName);
-              setUserEmail(user.email);
-              setUid(user.uid);
-            //   console.log(user.toJSON());
-            } else {
-              // User is signed out
-              // ...
-              console.log("user is logged out")
-            }
-          });
-         
-    }, []);   
-    
- 
-    return(
-        <>
-        <div className='bg-2 fade-in' id = 'background-2'/>
-                <Navbar/>
-                <div className='loading-bar load-fast'/>
-            
-            <div className='events-page-container'>
-                <p className='events-page-title'>{displayName}</p>
-                
-                <p className='events-page-title'>{userEmail}</p>
-                
-                <p className='events-page-title'>{uid}</p>
-            </div>
-            <Footer/>
-        </>
-    )
-}
- 
-export default Profile
+export default Profile;
